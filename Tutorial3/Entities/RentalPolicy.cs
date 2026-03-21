075bbbabb0dc92f48e0c41a37e07d3894d561a50
jaylate <178095084+jaylate@users.noreply.github.com>
2026-03-21 17:44:31 +0100

    feat(policy): Add policy class for penalty rules and user limits

---
+++ Tutorial3/Entities/RentalPolicy.cs
@@ -0,0 +1,7 @@
+namespace Tutorial3.Entities;
+
+public static class RentalPolicy {
+    public const int MAX_RENTAL_FOR_STUDENT = 2;
+    public const int MAX_RENTAL_FOR_EMPLOYEE = 5;
+    public const int OVERDUE_PENALTY_PER_DAY = 10;
+}
